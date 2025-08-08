8362863ad9cf2c3628ba360c6f608f38d4c18848
saboorcode <saboor2malik@gmail.com>
2025-08-07 22:32:46 -0700

    Displayed menu on homepage for customer view after pulling menu data from remote database

---
+++ js/index.js
@@ -0,0 +1,46 @@
+// Create a single supabase client for interacting with your database
+const supa = supabase.createClient('https:/tkmampaeiyfxilwhlobc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrbWFtcGFlaXlmeGlsd2hsb2JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyNjA5MzUsImV4cCI6MjA2OTgzNjkzNX0.zT7wt7_aaf15ae4sdFpOEL_Otqvw4DUiTlek0HCzYHo')
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const menuList = document.getElementById("menu-list")
+    
+    async function getMenu() {
+        
+        let { data: menu, error } = await supa
+            .from('menu')
+            .select('*')
+        
+
+        for (let i = 0; i < menu.length; i++){
+            const row = document.createElement("ul")
+
+            let li = document.createElement("li")
+            li.textContent = menu[i].dish
+
+            row.appendChild(li)
+
+            li = document.createElement("li")
+            li.textContent = menu[i].quarter_price
+
+            row.appendChild(li)
+
+            li = document.createElement("li")
+            li.textContent = menu[i].half_price
+
+            row.appendChild(li)
+
+            li = document.createElement("li")
+            li.textContent = menu[i].full_price
+
+            row.appendChild(li)
+
+            menuList.appendChild(row)
+        }
+
+    }
+
+    getMenu()
+          
+})
